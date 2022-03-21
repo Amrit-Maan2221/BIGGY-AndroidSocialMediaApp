52ce63975a2faaccb288c73f6a88c5337cc6dc3e
Amrit <AmritMaan2001@gmail.com>
2022-03-21 15:18:55 -0400

    minimal backend code for post activity added and also dependency of image cropper removed from graddle modules

--- app/src/main/java/com/example/biggy_social_media/PostActivity.java
+++ app/src/main/java/com/example/biggy_social_media/PostActivity.java
@@ -1,14 +1,52 @@
+// To do- Firebase setup left
+
+
 package com.example.biggy_social_media;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.hendraanggrian.appcompat.widget.SocialAutoCompleteTextView;
+import com.theartofdev.edmodo.cropper.CropImage;
 
 public class PostActivity extends AppCompatActivity {
+
+    private ImageView close;
+    private TextView post;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        close = findViewById(R.id.close);
+        post = findViewById(R.id.post);
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PostActivity.this , MainActivity.class));
+                finish();
+            }
+        });
+
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                upload();
+            }
+        });
+
+    }
+
+    private void upload() {
     }
 }
