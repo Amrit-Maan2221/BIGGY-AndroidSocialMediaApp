cffa230005fadc188ae124f0eb5038971094ee61
Amrit <AmritMaan2001@gmail.com>
2022-03-21 04:28:38 -0400

    Added on click listener on bottom nav with toast working on them

--- app/src/main/java/com/example/biggy_social_media/MainActivity.java
+++ app/src/main/java/com/example/biggy_social_media/MainActivity.java
@@ -8,13 +8,18 @@ Fix Bottom Navigation
 
 package com.example.biggy_social_media;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.biggy_social_media.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     ActivityMainBinding binding;
 
@@ -23,5 +28,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+
+    //Function Name: onOptionsItemSelected()
+    //Parameters: MenuItem item     -   menu item
+    //Description: This function is to implement the logic on menu item selected
+    //Return: boolean true if successful
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.home:
+                Toast.makeText(this, "home", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.search:
+                Toast.makeText(this, "search", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.notifications:
+                Toast.makeText(this, "notifications", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.person:
+                Toast.makeText(this, "profile", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void testing(View view) {
+        Toast.makeText(this, "testingWorkked", Toast.LENGTH_SHORT).show();
     }
 }
